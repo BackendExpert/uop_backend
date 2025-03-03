9184abedd335ca0c08b3d61d233351ee8ffb41ec
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-03 07:49:16 +0530

    Update

--- functions/login.php
+++ functions/login.php
@@ -1,8 +1,12 @@
 <?php 
     include "../config.php";
+    require "vendor/autoload.php";
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
 
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json");
+
 
     $data = json_decode(file_get_contents("php://input"), true);
 
@@ -15,10 +19,17 @@
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if($user && password_verify($password, $user['password'])){
-        echo json_encode(["message" => "Login successful", "status" => true]);
-    }
-    else{
+    if ($user && password_verify($password, $user['password'])) {
+        $payload = [
+            "id" => $user['id'],
+            "username" => $user['username'],
+            "exp" => time() + (60 * 60 * 24) 
+        ];
+        
+        $token = JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256');
+    
+        echo json_encode(["message" => "Login successful", "token" => $token]);
+    } else {
         echo json_encode(["message" => "Invalid credentials", "status" => false]);
     }
 ?>
---
+++ functions/verifyToken.php
@@ -0,0 +1,27 @@
+<?php
+    require "vendor/autoload.php";
+    include "config.php";
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+
+    $headers = getallheaders();
+
+    if (!isset($headers['Authorization'])) {
+        http_response_code(401);
+        echo json_encode(["message" => "Token missing"]);
+        exit();
+    }
+
+    $token = str_replace("Bearer ", "", $headers['Authorization']);
+
+    try {
+        $decoded = JWT::decode($token, new Key($_ENV['JWT_SECRET'], 'HS256'));
+        echo json_encode(["message" => "Token verified", "user" => $decoded]);
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(["message" => "Invalid token"]);
+    }
+?>
