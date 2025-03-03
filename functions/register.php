c8f4301c136c2dd9e333c95fa48a348c0e358877
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-03 07:19:03 +0530

    Update register.php

--- functions/register.php
+++ functions/register.php
@@ -8,8 +8,22 @@
 
     $username = $data['username'];
     $email = $data['email'];
-    $password = $data['password'];
+    $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
+    $checkuser_stmt = $pdo->prepare("SELECT * FROM users WHERE email=?");
+    $checkuser_stmt->execute([$email]);
+
+    // $user_indb = $checkuser_stmt->rowCount()
+
+    if($checkuser_stmt->rowCount() !== 0){
+        echo json_encode(["message" => "User Already in Database", "status" => false]);
+    }
+    else{
+        $insert_stmt = $pdo->prepare("INSERT INTO users (username, email, password) (?,?,?)");
+        $insert_stmt->execute([$username, $email, $password]);
+
+        echo json_encode(["message" => "User registered successfully"]);
+    }
     
 
 ?>
